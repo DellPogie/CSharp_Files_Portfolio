fceb88c4e5b5bc83058691712c5d570290b8254d
Delio Labaddan <149133144+DellPogie@users.noreply.github.com>
2023-12-02 15:09:36 +0800

    Add files via upload

---
+++ CSharp-Variables.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class MyVariables
+{
+	public static void Main()
+	{
+		int _int = 10;
+		byte _byte = 10;		
+		short _short = 10;
+		long _long = 10;
+		float _float = 10.1f;
+		double _double = 10.1d;
+		decimal _decimal = 10.1m;
+		char _char = 'A';
+		string _string = "A";
+		bool _bool = true;
+		
+		Console.WriteLine("int = " + _int);
+		Console.WriteLine("byte = " + _byte);
+		Console.WriteLine("short = " + _short);
+		Console.WriteLine("long = " + _long);
+		Console.WriteLine("floag = " + _float);
+		Console.WriteLine("double = " + _double);
+		Console.WriteLine("decimal = " + _decimal);
+		Console.WriteLine("char = " + _char);
+		Console.WriteLine("string = " + _string);
+		Console.WriteLine("bool = " + _bool);
+	}
+}
